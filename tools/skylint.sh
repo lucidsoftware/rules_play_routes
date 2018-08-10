4ac2e0cabcce5e1feddc03c1aa025e58547342d6
James Judd <james@lucidchart.com>
2018-08-09 23:33:26 -0600

    Fix Skylint script

--- tools/skylint.sh
+++ tools/skylint.sh
@@ -3,5 +3,5 @@
 echo "Running Skylint"
 skylint_path=src/tools/skylark/java/com/google/devtools/skylark/skylint
 bazel build @io_bazel//${skylint_path}:Skylint \
-  && find . -type f -name *.bzl \
+  && find . -type f -name '*.bzl' \
   | xargs "$(bazel info bazel-bin)/external/io_bazel/${skylint_path}/Skylint"
